c9cf8bb16e1dfa5096db47b677186c19db550b02
Klaus Sinani <klauscfhq@protonmail.com>
2019-01-14 07:09:53 +0200

    Added `help` module

---
+++ help.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = `
+  Usage
+    $ rels [<options> ...]
+
+    Options
+      --repo, -r         Repository to get analytics for
+      --number, -n       Number of releases to be displayed
+      --all, -a          Display all releases
+      --help, -h         Display help message
+      --version, -v      Display installed version
+
+    Examples
+      $ rels --repo klaussinani/tusk
+      $ rels --repo klaussinani/tusk --all
+      $ rels --repo klaussinani/tusk --number 3
+`;
